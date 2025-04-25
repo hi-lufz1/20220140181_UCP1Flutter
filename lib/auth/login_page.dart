ad437c85db7948813dc75eba5c6b1970b1853a5e
hi-lufz1 <latifufauzi@gmail.com>
2025-04-25 15:37:08 +0700

    menambahkan field untuk email dan password

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -11,28 +11,60 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/images/cosmo_square.png",
-              width: 100,
-              height: 100,
+        child: Form(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              spacing: 16.0,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/cosmo_square.png",
+                  width: 100,
+                  height: 100,
+                ),
+                const Text(
+                  "SELAMAT DATANG KEMBALI",
+                  style: TextStyle(fontSize: 28),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Email", style: TextStyle(fontSize: 16)),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        hintText: 'Email',
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    Text("Password", style: TextStyle(fontSize: 16)),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        hintText: 'Password',
+                      ),
+                      obscureText: true,
+                    ),
+                  ],
+                ),
+              ],
             ),
-            const Text(
-              "SELAMAT DATANG KEMBALI",
-              style: TextStyle(fontSize: 28),
-            ),
-
-          ],
+          ),
         ),
       ),
-
     );
   }
 }
