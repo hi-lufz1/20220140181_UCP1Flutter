61b3610885a9b2c0e39812dafd7c6d4aab7f3ebb
hi-lufz1 <latifufauzi@gmail.com>
2025-04-27 15:09:27 +0700

    menambahkan validasi dan memperbaiki tampilan input tanggal serta tugas

--- lib/piket/piket_page.dart
+++ lib/piket/piket_page.dart
@@ -33,6 +33,7 @@ class _PiketPageState extends State<PiketPage> {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
@@ -52,134 +53,168 @@ class _PiketPageState extends State<PiketPage> {
           style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Nama Anggota", style: TextStyle(fontSize: 16)),
-            TextFormField(
-              readOnly: true,
-              initialValue: widget.user,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text("Tanggal Piket", style: TextStyle(fontSize: 16)),
-            TextFormField(
-              controller: dateController,
-              readOnly: true,
-              decoration: InputDecoration(
-                prefixIcon: IconButton(
-                  icon: Icon(Icons.today_outlined),
-                  onPressed: () {
-                    showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(2000),
-                      lastDate: DateTime(2100),
-                    ).then((pickedDate) {
-                      if (pickedDate != null) {
-                        setState(() {
-                          dateController.text = DateFormat(
-                            'EEEE, dd MMMM yyyy',
-                            'id_ID',
-                          ).format(pickedDate);
-                        });
-                      }
-                    });
-                  },
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text("Tugas Piket", style: TextStyle(fontSize: 16)),
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: taskController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                    ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Nama Anggota", style: TextStyle(fontSize: 16)),
+              TextFormField(
+                readOnly: true,
+                initialValue: widget.user,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
                   ),
                 ),
-                SizedBox(width: 16.0),
-                Expanded(
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all<Color>(
-                        Colors.black,
-                      ),
-                      fixedSize: WidgetStateProperty.all(Size.fromHeight(50)),
-                    ),
+              ),
+              SizedBox(height: 16.0),
+              Text("Pilih Tanggal", style: TextStyle(fontSize: 16)),
+              TextFormField(
+                controller: dateController,
+                readOnly: true,
+                decoration: InputDecoration(
+                  hintText: 'Pilih Tanggal',
+                  prefixIcon: IconButton(
+                    icon: Icon(Icons.today_outlined),
                     onPressed: () {
-                      addPiket();
-                    },
-                    child: Text(
-                      "Tambah",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Daftar Tugas Piket",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Expanded(
-              child:
-                  piketList.isEmpty
-                      ? Center(
-                        child: Text(
-                          "Belum ada Data",
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      )
-                      : ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: piketList.length,
-                        itemBuilder: (context, index) {
-                          final piket = piketList[index];
-                          return Card(
-                            color: Color.fromRGBO(5, 169, 198, 1),
-                            margin: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: ListTile(
-                              title: Text(
-                                piket['tugas'] ?? '',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                      showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100),
+                        builder: (context, child) {
+                          return Theme(
+                            data: Theme.of(context).copyWith(
+                              colorScheme: ColorScheme.light(
+                                primary: Color.fromRGBO(5, 169, 198, 1),
+                                onPrimary: Colors.white,
+                                onSurface: Colors.black,
                               ),
-                              trailing: IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.arrow_forward_ios,
-                                  color: Colors.white,
-                                ),
+                              dialogTheme: DialogThemeData(
+                                backgroundColor: Colors.white,
                               ),
                             ),
+                            child: child!,
                           );
                         },
+                      ).then((pickedDate) {
+                        if (pickedDate != null) {
+                          setState(() {
+                            dateController.text = DateFormat(
+                              'EEEE, dd MMMM yyyy',
+                              'id_ID',
+                            ).format(pickedDate);
+                          });
+                        }
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Tanggal tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              Text("Tugas Piket", style: TextStyle(fontSize: 16)),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: taskController,
+                      decoration: InputDecoration(
+                        hintText: 'Tugas Piket',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
                       ),
-            ),
-          ],
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Tugas tidak boleh kosong';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(
+                          Colors.black,
+                        ),
+                        fixedSize: WidgetStateProperty.all(Size.fromHeight(50)),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          addPiket();
+                        }
+                      },
+                      child: Text(
+                        "Tambah",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Daftar Tugas Piket",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Expanded(
+                child:
+                    piketList.isEmpty
+                        ? Center(
+                          child: Text(
+                            "Belum ada Data",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        )
+                        : ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: piketList.length,
+                          itemBuilder: (context, index) {
+                            final piket = piketList[index];
+                            return Card(
+                              color: Color.fromRGBO(5, 169, 198, 1),
+                              margin: const EdgeInsets.symmetric(vertical: 8.0),
+                              child: ListTile(
+                                title: Text(
+                                  piket['tugas'] ?? '',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                trailing: IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+              ),
+            ],
+          ),
         ),
       ),
     );
