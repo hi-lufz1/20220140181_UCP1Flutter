9e0003d3d407344512669ab2b2a115f8217a0a27
hi-lufz1 <latifufauzi@gmail.com>
2025-04-27 16:00:02 +0700

    menambahkan field user tanggal dan tugas

--- lib/piket/detail_piket_page.dart
+++ lib/piket/detail_piket_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class DetailPiketPage extends StatelessWidget {
   final String user = 'User';
-  final String tanggal = '2023-10-01';  
+  final String tanggal = '2023-10-01';
   final String tugas = 'Tugas Piket';
 
   const DetailPiketPage({
@@ -33,7 +33,55 @@ class DetailPiketPage extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
-      
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  tanggal,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(5, 169, 198, 1),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(0, 224, 255, 1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    user,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              alignment: Alignment.centerLeft,
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(27, 27, 27, 1),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Text(
+                tugas,
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
