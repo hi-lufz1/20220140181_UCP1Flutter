6c6ca9ea99e59211bbf495674b83d8b15b22a737
hi-lufz1 <latifufauzi@gmail.com>
2025-04-27 15:28:32 +0700

    menambahan app bar detail piket

--- lib/piket/detail_piket_page.dart
+++ lib/piket/detail_piket_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
 class DetailPiketPage extends StatelessWidget {
-  const DetailPiketPage({super.key});
+  final String user = 'User';
+  final String tanggal = '2023-10-01';  
+  final String tugas = 'Tugas Piket';
+
+  const DetailPiketPage({
+    super.key,
+    // required this.user,
+    // required this.tanggal,
+    // required this.tugas,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Color.fromRGBO(0, 224, 255, 1),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          'Detail $tugas',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      
+    );
   }
 }
