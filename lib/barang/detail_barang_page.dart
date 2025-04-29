c980d54af971ba4d6349579d91ee756f29f75bdd
hi-lufz1 <latifufauzi@gmail.com>
2025-04-29 11:11:14 +0700

    memperbarui warna icon dan menambahkan tombol "Selesai"

--- lib/barang/detail_barang_page.dart
+++ lib/barang/detail_barang_page.dart
@@ -27,12 +27,12 @@ class DetailBarangPage extends StatelessWidget {
           children: [
             const SizedBox(height: 40),
             CircleAvatar(
-              radius: 60,
-              backgroundColor: Colors.green.shade100,
+              radius: 70,
+              backgroundColor:   Color.fromRGBO(200, 249, 255, 1),
               child: const Icon(
                 Icons.check_circle,
-                size: 80,
-                color: Colors.green,
+                size: 100,
+                color: Color.fromRGBO(0, 198, 224, 1),
               ),
             ),
             const SizedBox(height: 20),
@@ -48,20 +48,49 @@ class DetailBarangPage extends StatelessWidget {
                   _detail('Tanggal', tanggal),
                   _detail('Jenis Transaksi', jenisTransaksi),
                   _detail('Jenis Barang', jenisBarang),
-                  _detail('Jumlah Barang',  jumlahBarang.toString()),
+                  _detail('Jumlah Barang', jumlahBarang.toString()),
                   _detail('Jenis Harga Satuan', 'Rp. $hargaSatuan'),
                   _detail('Total Harga', 'Rp. $totalHarga'),
                 ],
               ),
             ),
             const SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(
+                          Color.fromRGBO(0, 224, 255, 1),
+                        ),
+                        fixedSize: WidgetStateProperty.all(Size.fromHeight(50)),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        int count = 0;
+                        Navigator.popUntil(context, (route) {
+                          return count++ == 2;
+                        });
+                      },
+                      child: Text("Selesai"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-   Widget _detail (String label, String value) {
+  Widget _detail(String label, String value) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 6),
       child: Column(
@@ -70,11 +99,17 @@ class DetailBarangPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(label, style: const TextStyle(fontSize: 16)),
-              Text(value, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              Text(
+                value,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ],
           ),
           SizedBox(height: 8),
-           const Divider(thickness: 1, height: 1, color: Colors.black12),
+          const Divider(thickness: 1, height: 1, color: Colors.black12),
           SizedBox(height: 8),
         ],
       ),
