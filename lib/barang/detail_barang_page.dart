35d9e1974276eba80ec027b8d82632ddc09fcdab
hi-lufz1 <latifufauzi@gmail.com>
2025-04-29 10:35:27 +0700

    menambahkan widget _detail untuk menampilkan informasi barang

--- lib/barang/detail_barang_page.dart
+++ lib/barang/detail_barang_page.dart
@@ -45,4 +45,24 @@ class DetailBarangPage extends StatelessWidget {
       ),
     );
   }
+
+   Widget _detail (String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(label, style: const TextStyle(fontSize: 16)),
+              Text(value, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          SizedBox(height: 8),
+           const Divider(thickness: 1, height: 1, color: Colors.black12),
+          SizedBox(height: 8),
+        ],
+      ),
+    );
+  }
 }
