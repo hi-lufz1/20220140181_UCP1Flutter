3ec8b315f2cd08adcdc4d17bc65a1f7f27d0de53
hi-lufz1 <latifufauzi@gmail.com>
2025-04-29 10:26:02 +0700

    menambahkan parameter dan fungsi hitung harga

--- lib/barang/detail_barang_page.dart
+++ lib/barang/detail_barang_page.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 class DetailBarangPage extends StatelessWidget {
-  const DetailBarangPage({super.key});
+  final String tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final int jumlahBarang;
+  final int hargaSatuan;
+  const DetailBarangPage({
+    super.key,
+    required this.tanggal,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.jumlahBarang,
+    required this.hargaSatuan,
+  });
+
+  int get totalHarga => jumlahBarang * hargaSatuan;
 
   @override
   Widget build(BuildContext context) {
